1108bda14b13c23db053253f4b2b19b42ff5c57c
allythiel <allythiel@gmail.com>
2021-05-10 20:27:31 -0500

    Add Random Destination

--- daytrip.js
+++ daytrip.js
@@ -1,1 +1,12 @@
 "Use Strict"
+
+let destinations = ["Mexico", "Italy", "Greece", "New Zealand", "Canada"];
+
+function randomDestination() {
+   for (let i = 0; i < destinations.length; i++) {
+        randomDes = destinations[Math.floor(Math.random() * destinations.length)];
+    }
+    console.log(randomDes);
+}
+
+randomDestination();
