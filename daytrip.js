b1ac3e8469d0290ceb408a6671f90000aa840879
allythiel <allythiel@gmail.com>
2021-05-10 21:35:57 -0500

    Reduced to one general function

--- daytrip.js
+++ daytrip.js
@@ -1,40 +1,17 @@
-"Use Strict"
+"use strict";
 
+let finalTrip = [];
 let destinations = ["Mexico", "Italy", "Greece", "New Zealand", "Canada"];
-
-function randomDestination() {
-    randomDes = destinations[Math.floor(Math.random() * destinations.length)];
-    return randomDes;
-}
-
-let myDestination = randomDestination();
-
-
 let restaurants = ["Kopps", "Pasta Tree", "Oakland Gyros", "Su Casa", "FreshFin"];
-
-function randomRestaurant() {
-    randomRest = restaurants[Math.floor(Math.random() * restaurants.length)];
-    return randomRest;
-}
-
-let myRestaurant = randomRestaurant();
-
-
 let transportation = ["Bus", "Taxi", "Train", "Walk", "Bike", "Boat"];
-
-function randomTransportation() {
-    randomTrans = transportation[Math.floor(Math.random() * transportation.length)];
-    return randomTrans;
-}
-
-let myTransportation = randomTransportation();
-
-
 let entertainment = ["Concert", "Hiking", "Shopping", "Bike Tour", "Go Swimming"];
 
-function randomEntertainment() {
-    randomEnt = entertainment[Math.floor(Math.random() * entertainment.length)];
-    return randomEnt;
+function randomEvent(array) {
+    randomItem = array[Math.floor(Math.random() * array.length)];
+    return randomItem;
 }
 
-let myEntertainment = randomEntertainment();
+let myDestination = randomDestination(array);
+let myRestaurant = randomRestaurant(array);
+let myTransportation = randomTransportation(array);
+let myEntertainment = randomEntertainment(array);
