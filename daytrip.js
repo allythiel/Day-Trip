94461c3ece4d53ba3cc86bf78d7ef291c220a045
allythiel <allythiel@gmail.com>
2021-05-11 21:57:58 -0500

    Add Call Function

--- daytrip.js
+++ daytrip.js
@@ -1,42 +1,48 @@
 "use strict";
 
 let finalTrip = [];
-let destinations = ["Mexico", "Italy", "Greece", "New Zealand", "Canada"];
-let restaurants = ["Kopps", "Pasta Tree", "Oakland Gyros", "Su Casa", "FreshFin"];
-let transportation = ["Bus", "Taxi", "Train", "Walk", "Bike", "Boat"];
-let entertainment = ["Concert", "Hiking", "Shopping", "Bike Tour", "Go Swimming"];
+let destinations = [" Mexico", " Italy", " Greece", " New Zealand", " Canada"];
+let restaurants = [" Kopps", " Pasta Tree", " Oakland Gyros", " Su Casa", " FreshFin"];
+let transportation = [" Bus", " Taxi", " Train", " Walk", " Bike", " Boat"];
+let entertainment = [" Concert", " Hiking", " Shopping", " Bike Tour", " Go Swimming"];
 
 function randomEvent(array) {
     let randomItem = array[Math.floor(Math.random() * array.length)];
     return randomItem;
 }
 
-let myDestination = randomEvent(destinations);
-let myRestaurant = randomEvent(restaurants);
-let myTransportation = randomEvent(transportation);
-let myEntertainment = randomEvent(entertainment);
+let myTripArr = randomArrayResults(destinations, restaurants, transportation, entertainment);
 
-
-function confirmDetails(string){
-    let operationComplete = false;
-    while(operationComplete === false){
-        let userConfirm = confirm("Press okay to confirm " + string);
-        operationComplete = userConfirm;
-    }
-    if (operationComplete === true) {
-        finalTrip.push(string);
-    }
+function randomArrayResults(destArr, restArr, transArr, entArr) {
+    let myDestination = randomEvent(destArr);
+    let myRestaurant = randomEvent(restArr);
+    let myTransportation = randomEvent(transArr);
+    let myEntertainment = randomEvent(entArr);
+    let resultArr = [myDestination, myRestaurant, myTransportation, myEntertainment];
+    return resultArr;
 }
 
-confirmDetails(myDestination);
-confirmDetails(myRestaurant);
-confirmDetails(myTransportation);
-confirmDetails(myEntertainment);
+function confirmDetails(array){
+    let operationComplete = false;
+    while(operationComplete === false){
+        let userConfirm = confirm("Press okay to confirm" + array);
+        operationComplete = userConfirm;
+        if (operationComplete === false) {
+            let myTripArr = randomArrayResults(destinations, restaurants, transportation, entertainment);
+            return myTripArr;
+        }
+    }
+    if (operationComplete === true) {
+        finalTrip.push(myTripArr);
+    }
+}
+confirmDetails(myTripArr);
+
 
 function confirmTrip(){
     let tripComplete = false;
     while (tripComplete === false){
-        let userAccepted = confirm("would you like to confirm a trip including " + finalTrip);
+        let userAccepted = confirm("Press OK to confirm your final trip which includes " + finalTrip);
         tripComplete = userAccepted;
     }
     if (tripComplete === true) {
